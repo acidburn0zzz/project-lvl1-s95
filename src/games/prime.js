97e4eb065e769aa7c5c100b6a14683988e8bfe07
iskaldvind <iskaldvind@gmail.com>
2017-04-29 17:48:56 +0300

    Step 9 fixes

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,32 +1,22 @@
 import gameFlow from '../';
 
 const description = 'Answer "yes" if number is prime otherwise answer "no".';
-const minDivisor = 7;
 
-const primeIter = (number, divisor, maxPossibledivisor) => {
-  if (divisor >= maxPossibledivisor) {
-    return 'yes';
+const isPrime = (number, currentDivisor, maxPossibledivisor) => {
+  if (currentDivisor >= maxPossibledivisor) {
+    return true;
   }
-  if (!(
-    (number % divisor) &&
-    (number % (divisor + 4)) &&
-    (number % (divisor + 6)) &&
-    (number % (divisor + 10)) &&
-    (number % (divisor + 12)) &&
-    (number % (divisor + 16)) &&
-    (number % (divisor + 22)) &&
-    (number % (divisor + 24)))) {
-    return 'no';
+  if (number % currentDivisor === 0) {
+    return false;
   }
-  return primeIter(number, divisor + 30, maxPossibledivisor);
+  return isPrime(number, currentDivisor + 1, maxPossibledivisor);
 };
 
 const genQuestionAndSolution = () => {
   const number = Math.floor((Math.random() * 98) + 3);
-  if (number % 2 === 0 || number % 3 === 0 || number % 5 === 0) {
-    return { question: number, correctAnswer: 'no' };
-  }
-  const correctAnswer = primeIter(number, minDivisor, Math.floor(Math.sqrt(number)));
+  const minPossibleDivisor = 2;
+  const maxPossibleDivisor = Math.floor(Math.sqrt(number));
+  const correctAnswer = isPrime(number, minPossibleDivisor, maxPossibleDivisor) ? 'yes' : 'no';
   return { question: number, correctAnswer };
 };
 
